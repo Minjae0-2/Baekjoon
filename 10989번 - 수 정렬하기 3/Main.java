603eb815a0b33087bc01a7827eb2e1bd9f710fe0
Minjae0-2 <lawpea38@gmail.com>
2025-07-28 21:02:55 +0900

    BOJ-EX: 7/28/2025, 9:02:53 PM

--- 10989번 - 수 정렬하기 3/Main.java
+++ 10989번 - 수 정렬하기 3/Main.java
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                      :::    :::    :::     */
-/*   Problem Number: 2869                              :+:    :+:      :+:    */
+/*   Problem Number: 10989                             :+:    :+:      :+:    */
 /*                                                    +:+    +:+        +:+   */
 /*   By: lawpea38 <boj.kr/u/lawpea38>                +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/2869                           #+#        #+#      #+#    */
-/*   Solved: 2025/07/28 20:38:00 by lawpea38      ###          ###   ##.kr    */
+/*   https://boj.kr/10989                          #+#        #+#      #+#    */
+/*   Solved: 2025/07/28 21:02:41 by lawpea38      ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 import java.io.BufferedReader;
@@ -16,13 +16,19 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
-        int a = Integer.parseInt(str[0]);
-        int b = Integer.parseInt(str[1]);
-        int v = Integer.parseInt(str[2]);
-        int day = (int)Math.ceil((double)(v-b)/(a-b));
-
-        System.out.println(day);
+        StringBuilder sb= new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[10001];
+        for(int i=0; i<n; i++){
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+        for(int j=1; j<=10000; j++){
+            while(arr[j]>0) {
+                sb.append(j).append('\n');
+                arr[j]--;
+            }
+        }
+        System.out.println(sb);
         br.close();
     }
 }
