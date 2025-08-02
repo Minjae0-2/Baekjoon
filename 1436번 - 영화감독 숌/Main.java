1f053b1989be272c823945dc2130edef3f839f4d
Minjae0-2 <lawpea38@gmail.com>
2025-08-02 16:55:58 +0900

    BOJ-EX: 8/2/2025, 4:55:57 PM

---
+++ 1436번 - 영화감독 숌/Main.java
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 1436                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: lawpea38 <boj.kr/u/lawpea38>                +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/1436                           #+#        #+#      #+#    */
+/*   Solved: 2025/08/02 16:55:24 by lawpea38      ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb= new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+        int answer= 665;
+        while(true){
+            if(n==count){
+                sb.append(answer);
+                break;
+            }
+            answer++;
+            if(String.valueOf(answer).contains("666")){
+                count++;
+            }
+        }
+        System.out.println(sb);
+
+        br.close();
+
+    }
+}
