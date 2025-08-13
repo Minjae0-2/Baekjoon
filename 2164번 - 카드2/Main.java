62a0d375acf3f70a023d354d074b55bf146fd690
Minjae0-2 <lawpea38@gmail.com>
2025-08-13 17:40:59 +0900

    BOJ-EX: 8/13/2025, 5:40:57 PM

---
+++ 2164번 - 카드2/Main.java
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 2164                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: lawpea38 <boj.kr/u/lawpea38>                +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/2164                           #+#        #+#      #+#    */
+/*   Solved: 2025/08/13 17:40:48 by lawpea38      ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Queue;
+import java.util.LinkedList;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> cardQueue = new LinkedList<>();
+        for(int i=1; i<=n; i++){
+            cardQueue.offer(i);
+        }
+        while(cardQueue.size()>1){
+            cardQueue.poll();
+            int add = cardQueue.poll();
+            cardQueue.offer(add);
+        }
+        System.out.print(cardQueue.poll());
+
+        br.close();
+    }
+}
+
